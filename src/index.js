0e7f8c37404683266fa25f91963d67d6e1fa67af
João Araújo <juauzaraujo@gmail.com>
2021-07-28 19:58:11 +0100

    update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize( process.env.REACT_APP_GA_TRACKING_CODE )
 
 
 ReactDOM.render(
